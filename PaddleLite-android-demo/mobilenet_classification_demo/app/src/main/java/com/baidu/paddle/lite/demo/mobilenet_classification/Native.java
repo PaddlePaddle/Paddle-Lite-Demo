2b6306ac537f8c9cd3e2f0be59ee3b8ef68a1542
hong19860320 <9973393+hong19860320@users.noreply.github.com>
2020-06-09 05:44:14 +0000

    [Android] Add mobilenet classification demo test=develop

---
+++ PaddleLite-android-demo/mobilenet_classification_demo/app/src/main/java/com/baidu/paddle/lite/demo/mobilenet_classification/Native.java
@@ -0,0 +1,58 @@
+package com.baidu.paddle.lite.demo.mobilenet_classification;
+
+import android.content.Context;
+
+public class Native {
+    static {
+        System.loadLibrary("Native");
+    }
+
+    private long ctx = 0;
+
+    public boolean init(String modelDir,
+                        String labelPath,
+                        int cpuThreadNum,
+                        String cpuPowerMode,
+                        int inputWidth,
+                        int inputHeight,
+                        float[] inputMean,
+                        float[] inputStd) {
+        ctx = nativeInit(
+                modelDir,
+                labelPath,
+                cpuThreadNum,
+                cpuPowerMode,
+                inputWidth,
+                inputHeight,
+                inputMean,
+                inputStd);
+        return ctx == 0;
+    }
+
+    public boolean release() {
+        if (ctx == 0) {
+            return false;
+        }
+        return nativeRelease(ctx);
+    }
+
+    public boolean process(int inTextureId, int outTextureId, int textureWidth, int textureHeight, String savedImagePath) {
+        if (ctx == 0) {
+            return false;
+        }
+        return nativeProcess(ctx, inTextureId, outTextureId, textureWidth, textureHeight, savedImagePath);
+    }
+
+    public static native long nativeInit(String modelDir,
+                                         String labelPath,
+                                         int cpuThreadNum,
+                                         String cpuPowerMode,
+                                         int inputWidth,
+                                         int inputHeight,
+                                         float[] inputMean,
+                                         float[] inputStd);
+
+    public static native boolean nativeRelease(long ctx);
+
+    public static native boolean nativeProcess(long ctx, int inTextureId, int outTextureId, int textureWidth, int textureHeight, String savedImagePath);
+}
