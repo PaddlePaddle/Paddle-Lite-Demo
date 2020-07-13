611f754a1d662b21847cbdeb063e3f904fcb0583
lvxiangxiang <lvxiangxiang@baidu.com>
2020-07-13 15:25:21 +0800

    ios ocr demo

--- PaddleLite-ios-demo/download_dependencies.sh
+++ PaddleLite-ios-demo/download_dependencies.sh
@@ -3,8 +3,10 @@ set -e
 
 CLASSIFICATION_MODEL_URL="https://paddlelite-demo.bj.bcebos.com/models/mobilenet_v1_fp32_224_for_cpu_v2_6_0.tar.gz"
 DETECTION_MODEL_URL="https://paddlelite-demo.bj.bcebos.com/models/ssd_mobilenet_v1_pascalvoc_fp32_300_for_cpu_v2_6_0.tar.gz"
+OCR_MODEL_URL="https://paddleocr.bj.bcebos.com/deploy/lite/ocr_v1_for_cpu.tar.gz"
 PADDLE_LITE_LIB_URL="https://paddlelite-demo.bj.bcebos.com/libs/ios/paddle_lite_libs_v2_6_0.tar.gz"
 OPENCV_FRAMEWORK_URL="https://paddlelite-demo.bj.bcebos.com/libs/ios/opencv2.framework.tar.gz"
+OPENCV3_FRAMEWORK_URL="https://paddlelite-demo.bj.bcebos.com/libs/ios/opencv3.framework.tar.gz"
 
 download_and_extract() {
     local url="$1"
@@ -40,3 +42,9 @@ download_and_extract "${PADDLE_LITE_LIB_URL}" "./ios-detection_demo/detection_de
 download_and_extract "${OPENCV_FRAMEWORK_URL}" "./ios-detection_demo/detection_demo"
 echo -e "[done]"
 
+# for ocr demo
+echo -e "[Download ios ocr demo denpendancy]\n"
+download_and_extract "${OCR_MODEL_URL}" "./ios-ocr_demo/ocr_demo/models"
+download_and_extract "${PADDLE_LITE_LIB_URL}" "./ios-ocr_demo/ocr_demo"
+download_and_extract "${OPENCV3_FRAMEWORK_URL}" "./ios-ocr_demo/ocr_demo"
+echo -e "[done]\n"
