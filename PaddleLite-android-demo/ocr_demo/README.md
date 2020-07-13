a50eda1c6063090115fe3e021b015fda1c9d1af6
fujiayi <fujiayi@baidu.com>
2020-07-13 11:31:04 +0800

    Add Android OCR Demo

---
+++ PaddleLite-android-demo/ocr_demo/README.md
@@ -0,0 +1,8 @@
+# HOW To Test This Android Demo
+### 1. Install or upgrade latest version of Android Studio , for ex. from https://developer.android.com/studio
+
+### 2. If you are a beginner, try to test a simple C++ NDK project.
+Follow instructions in Android Studio: File -> New ->New Project， find a “Native C++” project
+
+### 3. Import this project.
+Follow instructions in Android Studio: File->New->Import Project...
---
+++ PaddleLite-android-demo/ocr_demo/app/src/main/cpp/preprocess.h
@@ -0,0 +1,17 @@
+//
+// Created by fujiayi on 2020/7/3.
+//
+#pragma once
+
+#include <jni.h>
+#include <opencv2/opencv.hpp>
+#include "common.h"
+cv::Mat bitmap_to_cv_mat(JNIEnv *env, jobject bitmap);
+
+cv::Mat resize_img(const cv::Mat& img, int height, int width);
+
+void neon_mean_scale(const float* din,
+                     float* dout,
+                     int size,
+                     const std::vector<float>& mean,
+                     const std::vector<float>& scale);
