af60c15179a8d345b2d2c8e5846a64cc0d8c2314
chenjiaoAngel <chenjiaobuaa@126.com>
2021-12-10 18:24:47 +0800

    add app_shell demo

---
+++ ocr/android/shell/ocr_demo/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# push
+adb push ./ocr_demo_exec /data/local/tmp/
+ocr_demo_path="/data/local/tmp/ocr_demo_exec"
+
+# run
+adb shell "cd ${ocr_demo_path} \
+           && chmod +x ./pipeline \
+           && export LD_LIBRARY_PATH=${ocr_demo_path}:${LD_LIBRARY_PATH} \
+           && ./pipeline \
+                ./models/ch_ppocr_mobile_v2.0_det_slim_opt.nb \
+                ./models/ch_ppocr_mobile_v2.0_rec_slim_opt.nb \
+                ./models/ch_ppocr_mobile_v2.0_cls_slim_opt.nb \
+                ./images/test.jpg \
+                ./test_img_result.jpg \
+                ./labels/ppocr_keys_v1.txt \
+                ./config.txt"
+
+adb pull ${ocr_demo_path}/test_img_result.jpg .
