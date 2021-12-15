4cf95403d70681cd25b68b528b2b8e210c6e4419
chenjiaoAngel <chenjiaobuaa@126.com>
2021-12-15 14:30:05 +0800

    fix build err.

--- ocr/linux/shell/cxx/ppocr_demo/build.sh
+++ ocr/linux/shell/cxx/ppocr_demo/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# build
-cd $(pwd)/src
 # configure
 ARM_ABI=armv8
 # ARM_ABI=armv7hf
-PADDLE_LITE_DIR="$(pwd)/../../../../../libs/linux/cxx"
+PADDLE_LITE_DIR="$(pwd)/../../../../../libs/linux"
+# build
+cd $(pwd)/src
 
 if [ "x$1" != "x" ]; then
     ARM_ABI=$1
@@ -22,17 +22,16 @@ make
 echo "make successful!"
 
 # mkdir
-cd ..
+cd ../../
 if [ ! -d "./ppocr_demo" ]; then
 mkdir ppocr_demo
-fi
+fils
 
 cp ./src/ppocr_demo ./ppocr_demo
-cp -r ../../../assets/config.txt ./ppocr_demo
-cp -r ../../../assets/models ./ppocr_demo
-cp -r ../../../assets/labels ./ppocr_demo
-cp -r ../../../assets/images ./ppocr_demo
-cp ${PADDLE_LITE_DIR}/libs/${ARM_ABI}/libc++_shared.so ./ppocr_demo
+cp -r ../../../../assets/config.txt ./ppocr_demo
+cp -r ../../../../assets/models ./ppocr_demo
+cp -r ../../../../assets/labels ./ppocr_demo
+cp -r ../../../../assets/images ./ppocr_demo
 cp ${PADDLE_LITE_DIR}/libs/${ARM_ABI}/libpaddle_light_api_shared.so ./ppocr_demo
 
 echo "copy successful!"
