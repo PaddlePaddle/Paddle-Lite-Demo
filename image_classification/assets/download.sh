e043a505a0089f3cc9fc70d8049065cf97b7a33a
chenjiaoAngel <chenjiaobuaa@126.com>
2022-01-10 11:59:28 +0800

    fix comment

--- image_classification/assets/download.sh
+++ image_classification/assets/download.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
-IMAGES_URL="https://paddlelite-demo.bj.bcebos.com/images/CLS/image.tar.gz"
-LABELS_URL="https://paddlelite-demo.bj.bcebos.com/labels/CLS/labels.tar.gz"
-MODEL_URL="https://paddlelite-demo.bj.bcebos.com/models/CLS/mobilenet_v1_for_cpu.tar.gz"
+MODEL_URL="https://paddlelite-demo.bj.bcebos.com/demo/image_classification/models/mobilenet_v1_for_cpu_v2_10.tar.gz"
 
 MODELS_DIR="$(pwd)/models/"
-IMAGES_DIR="$(pwd)/images/"
-LABELS_DIR="$(pwd)/labels/"
 
 if [ ! -d "$(pwd)/models" ]; then
   mkdir $(pwd)/models
-fi
-if [ ! -d "$(pwd)/images" ]; then
-  mkdir $(pwd)/images
-fi
-
-if [ ! -d "$(pwd)/labels" ]; then
-  mkdir $(pwd)/labels
 fi
 
 download_and_uncompress() {
@@ -32,7 +21,5 @@ download_and_uncompress() {
 }
 
 download_and_uncompress "${MODEL_URL}" "${MODELS_DIR}"
-download_and_uncompress "${IMAGES_URL}" "${IMAGES_DIR}"
-download_and_uncompress "${LABELS_URL}" "${LABELS_DIR}"
 
 echo "Download successful!"
--- ocr/assets/download.sh
+++ ocr/assets/download.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-IMAGES_URL="https://paddlelite-demo.bj.bcebos.com/images/OCR/images.tar.gz"
-LABELS_URL="https://paddlelite-demo.bj.bcebos.com/labels/OCR/labels.tar.gz"
-CLS_MODEL_URL="https://paddlelite-demo.bj.bcebos.com/models/ch_ppocr_mobile_v2.0_cls_slim_opt_for_cpu_v2_10_rc.tar.gz"
-DET_MODEL_URL="https://paddlelite-demo.bj.bcebos.com/models/ch_ppocr_mobile_v2.0_det_slim_opt_for_cpu_v2_10_rc.tar.gz"
-REC_MODEL_URL="https://paddlelite-demo.bj.bcebos.com/models/ch_ppocr_mobile_v2.0_rec_slim_opt_for_cpu_v2_10_rc.tar.gz"
-CONFIG_TXT_URL="https://paddlelite-demo.bj.bcebos.com/labels/OCR/config.tar.gz"
+IMAGES_URL="https://paddlelite-demo.bj.bcebos.com/demo/ocr/images/images.tar.gz"
+LABELS_URL="https://paddlelite-demo.bj.bcebos.com/demo/ocr/labels/OCR/labels.tar.gz"
+CLS_MODEL_URL="https://paddlelite-demo.bj.bcebos.com/demo/ocr/models/ch_ppocr_mobile_v2.0_cls_slim_opt_for_cpu_v2_10_rc.tar.gz"
+DET_MODEL_URL="https://paddlelite-demo.bj.bcebos.com/demo/ocr/models/ch_ppocr_mobile_v2.0_det_slim_opt_for_cpu_v2_10_rc.tar.gz"
+REC_MODEL_URL="https://paddlelite-demo.bj.bcebos.com/demo/ocr/models/ch_ppocr_mobile_v2.0_rec_slim_opt_for_cpu_v2_10_rc.tar.gz"
 
 if [ ! -d "$(pwd)/models" ]; then
  mkdir $(pwd)/models
@@ -20,7 +19,6 @@ fi
 MODELS_DIR="$(pwd)/models/"
 IMAGES_DIR="$(pwd)/images/"
 LABELS_DIR="$(pwd)/labels/"
-CONFIG_DIR="$(pwd)"
 
 download_and_uncompress() {
   local url="$1"
@@ -40,6 +38,5 @@ download_and_uncompress "${DET_MODEL_URL}" "${MODELS_DIR}"
 download_and_uncompress "${REC_MODEL_URL}" "${MODELS_DIR}"
 download_and_uncompress "${IMAGES_URL}" "${IMAGES_DIR}"
 download_and_uncompress "${LABELS_URL}" "${LABELS_DIR}"
-download_and_uncompress "${CONFIG_TXT_URL}" "${CONFIG_DIR}"
 
 echo "Download successful!"
