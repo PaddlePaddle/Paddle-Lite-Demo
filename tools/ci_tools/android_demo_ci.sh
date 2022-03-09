91c59cfe643975bfeacbfa96a832a56b008ac1b2
devchai123 <986828121@qq.com>
2022-03-09 10:45:10 +0800

    test

---
+++ tools/ci_tools/android_demo_ci.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -ex
+
+# Set proxy if exists
+if [ -n "$PROXY" ]; then
+     export http_proxy=$PROXY
+     export https_proxy=$PROXY∂
+fi
+if [ -n "$NO_PROXY" ]; then
+     export no_proxy=$NO_PROXY∑
+fi
+cd .. && rm -rf Paddle-Lite-Demo
+
+####################################################################################################
+# 1. functions of prepare workspace before compiling
+####################################################################################################
+# 1.1 compile paddlelite android lib
+function compile_lib {
+    git clone https://github.com/PaddlePaddle/Paddle-Lite-Demo.git && cd Paddle-Lite-Demo
+    git branch -a && git checkout develop
+    chmod +x ./libs/download.sh
+    bash ./libs/download.sh
+    bash ./object_detection/assets/download.sh
+    sed -i '3s/disk/opt/g' ./object_detection/android/shell/cxx/picodet_detection/build.sh
+    cd object_detection/android/shell/cxx/picodet_detection && bash build.sh
+}
+
+
+####################################################################################################
+# 4. main function
+#    $1: adb device index, eg. 0 1
+#    $2: adb workspace directory name
+####################################################################################################
+
+function main {
+  # step1. compile paddle-lite-demo android lib
+  compile_lib
+
+}
+
+main
