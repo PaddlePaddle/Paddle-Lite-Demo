3867db8871aab02c99d2ac9207493752ada3f8fa
mjp9527 <1821582672@qq.com>
2022-03-30 15:14:15 +0800

    tmp save

---
+++ human_segmentation/android/shell/cxx/human_segmentation/run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+PADDLE_LITE_DIR="$(pwd)/../../../../../libs/android/cxx"
+OPENCV_LITE_DIR="$(pwd)/../../../../../libs/android/opencv4.1.0"
+ASSETS_DIR="$(pwd)/../../../../assets"
+ADB_DIR="/data/local/tmp/human_segmentation"
+ARM_ABI=$1 # arm64-v8a or armeabi-v7a
+
+echo "PADDLE_LITE_DIR is ${PADDLE_LITE_DIR}"
+echo "OPENCV_LITE_DIR is ${OPENCV_LITE_DIR}"
+echo "ASSETS_DIR is ${ASSETS_DIR}"
+echo "ADB_DIR is ${ADB_DIR}"
+# mkdir
+adb -s 4c4f947c shell "cd /data/local/tmp/ && mkdir human_segmentation"
+# push
+adb -s 4c4f947c push ./build/human_segmentation ${ADB_DIR}
+adb -s 4c4f947c push ${ASSETS_DIR}/models/ ${ADB_DIR}
+adb -s 4c4f947c push ${ASSETS_DIR}/images/ ${ADB_DIR}
+adb -s 4c4f947c push ${ASSETS_DIR}/labels/ ${ADB_DIR}
+adb -s 4c4f947c push ${PADDLE_LITE_DIR}/libs/${ARM_ABI}/libc++_shared.so  ${ADB_DIR}
+adb -s 4c4f947c push ${PADDLE_LITE_DIR}/libs/${ARM_ABI}/libpaddle_light_api_shared.so  ${ADB_DIR}
+
+# run
+echo "--run model on cpu---"
+adb -s 4c4f947c shell "cd ${ADB_DIR} \
+           && chmod +x ./human_segmentation \
+           && export LD_LIBRARY_PATH=${ADB_DIR}:${LD_LIBRARY_PATH} \
+           &&  ./human_segmentation \
+               ./models/model.nb   \
+               ./images/human.jpg  \
+               ./labels/label_list \
+               513 513 4    \
+               0 10 10 0    \
+               "
