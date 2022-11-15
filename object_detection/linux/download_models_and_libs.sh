d9788ff4eaa8f14ca6aea89592633b6950eb57f6
heyanru01 <429520051@qq.com>
2022-11-15 15:27:46 +0800

    change model dir

--- object_detection/linux/download_models_and_libs.sh
+++ object_detection/linux/download_models_and_libs.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-DETECTION_MODEL_DIR="$(pwd)/picodet_detection/models"
+DETECTION_MODEL_DIR="../assets/models"
 LIBS_DIR="$(pwd)"
 
 DETECTION_MODEL_URL="https://paddlelite-demo.bj.bcebos.com/Paddle-Lite-Demo/models/picodetv2_relu6_coco_no_fuse.tar.gz"
