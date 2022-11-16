c86eb60ee4c5969f51ee57a00533b1158a40d2f6
heyanru01 <429520051@qq.com>
2022-11-16 05:49:32 +0000

    test7

--- object_detection/linux/picodet_detection/run.sh
+++ object_detection/linux/picodet_detection/run.sh
@@ -12,4 +12,4 @@ export VSI_NN_LOG_LEVEL=0
 export VIV_VX_ENABLE_GRAPH_TRANSFORM=-pcq:1
 export VIV_VX_SET_PER_CHANNEL_ENTROPY=100
 export TIMVX_BATCHNORM_FUSION_MAX_ALLOWED_QUANT_SCALE_DEVIATION=30000
-build/object_detection_demo ../../assets/models/picodetv2_relu6_coco_no_fuse ../../assets/labels/coco_label_list.txt models/picodetv2_relu6_coco_no_fuse/subgraph.txt models/picodetv2_relu6_coco_no_fuse/picodet.yml 
+build/object_detection_demo ../../assets/models/picodetv2_relu6_coco_no_fuse ../../assets/labels/coco_label_list.txt ../../assets/models/picodetv2_relu6_coco_no_fuse/subgraph.txt ../../assets/models/picodetv2_relu6_coco_no_fuse/picodet.yml 
